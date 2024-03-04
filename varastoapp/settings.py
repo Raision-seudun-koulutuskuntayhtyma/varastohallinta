7a7d69ec0801d47fcbe3ec3bfe15212430b6e2b6
Tuomas Suutari <tuomas@nepnep.net>
2024-03-04 15:00:27 +0200

    settings: Vaihda suomalainen kieli ja aikavyöhyke

--- varastoapp/settings.py
+++ varastoapp/settings.py
@@ -116,10 +116,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-# LANGUAGE_CODE = 'fi'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fi'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Helsinki'
 
 USE_I18N = True
 
